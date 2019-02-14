8b7493b9d64ae3a8360bba159676ff3b7ba845fa
Szalona <44002942+Szalona@users.noreply.github.com>
2019-02-14 09:45:27 +0100

    Update Credentials.java

--- Practice/src/main/java/setup/Credentials.java
+++ Practice/src/main/java/setup/Credentials.java
@@ -2,7 +2,7 @@ package setup;
 
 public class Credentials {
     public static final String USERNAME_EWA_TESTOWA = "...";
-    public static final String PASSWORD_EWA_TESTOWA = "...k";
+    public static final String PASSWORD_EWA_TESTOWA = "...";
 
 
 }
