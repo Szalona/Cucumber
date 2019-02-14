facc3413da4bffe6eacf3d7233c2c80167a6d752
Szalona <44002942+Szalona@users.noreply.github.com>
2019-02-14 09:36:50 +0100

    Add files via upload

---
+++ ontalent/src/test/java/steps/LoginSteps.java
@@ -0,0 +1,12 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import ontalentbase.TestConfigurationBase;
+
+public class LoginSteps extends TestConfigurationBase {
+
+    @Given("^login to platform as Ewa Testowa user$")
+    public void loginToPlatformAsEwaTestowaUser() {
+
+    }
+}
