94c43d0e90a9bd3b61801e6a901f24397126021e
Szalona <44002942+Szalona@users.noreply.github.com>
2019-02-14 09:45:17 +0100

    Update Credentials.java

--- Practice/src/main/java/setup/Credentials.java
+++ Practice/src/main/java/setup/Credentials.java
@@ -1,8 +1,8 @@
 package setup;
 
 public class Credentials {
-    public static final String USERNAME_EWA_TESTOWA = "ewa.tusinska@onwelo.com";
-    public static final String PASSWORD_EWA_TESTOWA = "cokolwiek";
+    public static final String USERNAME_EWA_TESTOWA = "...";
+    public static final String PASSWORD_EWA_TESTOWA = "...k";
 
 
 }
